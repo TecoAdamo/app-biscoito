c52c4ec706605ee5010f8d3356b6025d41fb931e
Mateus Adamo <tecoadamo2003@gmail.com>
2023-08-17 17:09:38 -0300

    App Biscoito da sorte simples

--- App.js
+++ App.js
@@ -1,13 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: ' ',
+      img: require('./src/biscoito.png')
+
+    }
+    
+    this.quebraBiscoito = this.quebraBiscoito.bind(this)
+
+    this.frases = [
+      'Há ainda tanta coisa linda na vida para se descobrir.',
+      'Nós somos como o clima: feitos de dias de chuva e de sol!',
+      'Somos capazes de fazer muito mais do que imaginamos!',
+      'A palavra “impossível” foi inventada para ser desafiada.',
+      'Abra os olhos e aventure-se nessa jornada chamada vida!',
+      'Que todos os nossos passos nos levem para o que nos faz feliz de verdade.',
+      'Os dias difíceis são a forma mais rápida de aprendermos a apreciar os bons.',
+      'Hoje é um novo dia. Não fique pensando no ontem.',
+      'A vitalidade é demonstrada não apenas pela persistência, mas pela capacidade de começar de novo.',
+      'Os sonhos são a melhor forma de termos uma direção na vida e um motivo para caminhar.',
+
+    ]
+  }
+
+  quebraBiscoito(){
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
+
+    this.setState({
+      textoFrase: ' " ' + this.frases[numeroAleatorio] + ' " ',
+      img: require('./src/biscoitoAberto.png')
+    })
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Image
+        source={this.state.img}
+        style={styles.img}
+        />
+
+        <Text style={styles.textFrase}>{this.state.textoFrase}</Text>
+        <TouchableOpacity style={styles.botão} onPress={this.quebraBiscoito}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnText}>Quebrar Biscoito</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +69,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img:{
+    width:240,
+    height: 240,
+  },
+  textFrase:{
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  botão:{
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 20,
+  },
+  btnArea:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText:{
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
+
 });
+export default App;
